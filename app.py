1247d90d82aeca6ae2f95e1f54fea3e63ce0a51a
Jérôme Mahuet <rydgel@ymail.com>
2012-03-02 16:00:47 +0100

    logging to stdout

--- app.py
+++ app.py
@@ -77,9 +77,10 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     # logging
     if not app.debug: 
-        handler = logging.StreamHandler(sys.__stdout__) 
-        handler.setLevel(logging.INFO) 
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
         app.logger.addHandler(handler)
+        
         sentry = Sentry(app)
         
     app.run(host='0.0.0.0', port=port)
